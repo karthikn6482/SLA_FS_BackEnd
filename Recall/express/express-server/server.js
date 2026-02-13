94010cc6405e1e92a94756d4c02f5b126a61227c
Karthi <karthikn6482@gmail.com>
2026-02-13 06:24:28 +0530

    Initial commit

---
+++ Recall/express/express-server/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
